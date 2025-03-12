c8f616d91d03ca6d045e676a2c8abf85a00370d0
elikamm <mail@eliaskamm.com>
2025-03-12 11:02:24 +0100

    implement fitness calculators

--- src/modular_fitness_calculator.cpp
+++ src/modular_fitness_calculator.cpp
@@ -11,7 +11,17 @@ void AbsoluteFitnessCalculator::calculate(Population& population, const Instance
 
 void RelativeFitnessCalculator::calculate(Population& population, const Instance& instance) const
 {
-    (void) population;
-    (void) instance;
-    error("TODO: Implement RelativeFitnessCalculator");
+    int worst_cost = 0;
+
+    for (auto& solution : population.solutions)
+    {
+        int cost = solution.get_cost(instance);
+
+        if (cost > worst_cost) worst_cost = cost;
+    }
+
+    for (Solution& solution : population.solutions)
+    {
+        solution.fitness = worst_cost - solution.get_cost(instance);
+    }
 }
